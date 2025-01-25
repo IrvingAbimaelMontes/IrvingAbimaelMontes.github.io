30f7358f2921379d48a7c27a98b4ac625607ed18
IrvingAbimaelMontes <irving.monteszapata@cesunbc.edu.mx>
2025-01-25 15:28:48 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirigiacd() {
+    window.location.href = "CineciadeDatos.html";
+}
+
+function redirigiapandas() {
+    window.location.href = "Pandas.html";
+}
+
+function home() {
+    window.location.href = "index.html";
+}
